7bfa1f011ea9ef5c6e7394c1896b6d2657fa90fe
monish <monishgowda27@gmail.com>
2026-02-11 15:36:14 +0530

    added tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,12 @@
+a=(1,2,3,4)
+print(type(a))
+#accessing tuple
+print(a[1])
+print(a[0:3])
+#concatenation
+b=(5,6,7)
+print("cancatenated:",a+b)
+#repetition
+print("repetition:",a*2)
+#length
+print(len(a))
