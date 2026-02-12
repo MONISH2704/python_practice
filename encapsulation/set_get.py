5777616d7b6ad753eaa507d526ff3f3638f9267a
monish <monishgowda27@gmail.com>
2026-02-12 15:12:24 +0530

    modified encapsulation

--- encapsulation/set_get.py
+++ encapsulation/set_get.py
@@ -1,11 +1,11 @@
 class encaps:
-    def __init__(self):
-        self.__balance=100
+    def __init__(self,bal):
+        self.__balance=bal
     def get_balance(self):
         print("the balance is :",self.__balance)
     def set_balance(self,amt):
         self.__balance+=amt
-a=encaps()
+a=encaps(500)
 a.get_balance()
 a.set_balance(100)
 a.get_balance()
